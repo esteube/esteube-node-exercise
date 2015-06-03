b45feabf68d3772124109b0fc77901fc4b1aedb3
Ellen Steube <ellensteube@gmail.com>
2015-06-03 11:32:13 -0600

    added node exercises

---
+++ app.js
@@ -0,0 +1,7 @@
+var foo = require('./lib.js');
+
+console.log(foo.sum(7));
+
+console.log(foo.subtract(4));
+
+console.log(foo.multiply(8));
---
+++ lib.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+sum: function(num){
+  return num + num;
+},
+
+subtract: function(num){
+  return num - num;
+},
+
+multiply: function(num){
+  return num * num;
+}
+
+};
